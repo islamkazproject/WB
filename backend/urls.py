05f2345aecba15ee37a3a5ccf827bb50eafbfc75
islam <islamkazproject@gmail.com>
2024-03-07 14:00:01 +0300

    Added auto-documentation(swagger/redoc) and jwt

--- backend/urls.py
+++ backend/urls.py
@@ -1,19 +1,22 @@
-
-
-from django.urls import path, include
+from django.urls import path, include, re_path
 from backend.views import (
     ServiceViewSet,
     ReviewViewSet,
     ScheduleViewSet,
 )
 from rest_framework import routers
+from react_django.yasg import urlpatterns as doc_url
 
 router = routers.DefaultRouter()
 router.register(r'services', ServiceViewSet)
 router.register(r'reviews', ReviewViewSet)
 router.register(r'schedules', ScheduleViewSet)
 
-
 urlpatterns = [
-    path('api/v1/', include(router.urls))
+    path('api/v1/', include(router.urls)),
+    path('api/v1/auth/', include('djoser.urls')),
+    re_path('api/v1/auth/', include('djoser.urls.authtoken')),
+    path('api/v1/auth/', include('djoser.urls.jwt')),
 ]
+
+urlpatterns += doc_url
