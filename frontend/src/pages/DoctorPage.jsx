870f8fb745cbd9d414828decc86992bf816cc467
bulat <bulat.sabitov.82@mail.ru>
2024-03-09 13:24:02 +0300

    add calendare for doctors

--- frontend/src/pages/DoctorPage.jsx
+++ frontend/src/pages/DoctorPage.jsx
@@ -1,15 +1,24 @@
 import "../styles/DoctorPage.css"
-import Footer from "../components/Footer/Footer";
 import React from "react";
+import DoctorScheduleChart from "../components/Schedle/ScheduleChart";
 const DoctorPage = () => {
+    const scheduleData = [
+        { day: "Monday", hours: 1 },
+        { day: "Tuesday", hours: 1 },
+        { day: "Wednesday", hours: 1 },
+        { day: "Thursday", hours: 1 },
+        { day: "Friday", hours: 1 },
+        { day: "Saturday", hours: 1 }
+    ];
     return (
+
         <div>
             <header>
                 <h1>Страница врача</h1>
             </header>
             <main>
                 <section className="doctor-info">
-                    {/*<img src="doctor.jpg" alt="Фото врача" />*/}
+                    <img src="doctor.jpg" alt="Фото врача" />
                     <h2>ФИО врача</h2>
                     <p>Специализация: <em>Стоматолог-терапевт</em></p>
                     <p>Должность: <em>Врач первой категории</em></p>
@@ -18,8 +27,9 @@ const DoctorPage = () => {
                     <p>Сертификаты: <em>"Современные методы лечения кариеса", "Эндодонтия"</em></p>
                 </section>
                 <section className="schedule">
-                    <div className="calendar"></div>
-
+                    <div className="calendar">
+                        <DoctorScheduleChart />
+                    </div>
                 </section>
                 <section className="records">
                     <h3>Записи на прием</h3>
